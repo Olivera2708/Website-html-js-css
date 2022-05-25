15c0ad6d103e5a9fe1f52334abeffbc21d9c7083
Olivera2708 <>
2022-05-25 17:22:22 +0200

    added js repertoar

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ let prijava_popup = document.getElementById('pop-up-prijava');
 let registracija_popup = document.getElementById('pop-up-reg')
 let pozadina = document.getElementById('transparent')
 
-var pozorista = null;
-
 kartice = document.getElementsByClassName("cards-lg")[0]
 
 let url_korisnici = "https://web-dizajn-a94be-default-rtdb.firebaseio.com/korisnici.json";
@@ -28,6 +26,7 @@ request_pozorista.onreadystatechange = function () {
   if (this.readyState == 4) {
     if (this.status == 200) {
       pozorista = JSON.parse(this.responseText);
+      localStorage.setItem("pozorista", JSON.stringify(pozorista));
       if (document.URL.includes("index.html")) {
         prikazi_pozorista(pozorista);
       }
@@ -42,6 +41,9 @@ request_predstave.onreadystatechange = function () {
   if (this.readyState == 4) {
     if (this.status == 200) {
       predstave = JSON.parse(this.responseText);
+      if (document.URL.includes("pozoriste.html")){
+        prikazi_predstave(predstave, localStorage.getItem("izabrano_pozoriste"));
+      }
     }
   }
 };
@@ -49,15 +51,13 @@ request_predstave.open("GET", url_predstave);
 request_predstave.send();
 
 
-//index.hmtl
-console.log(pozorista); //NE RADI MI KAO GLOBAL
-
+//index.html
 function prikazi_pozorista(pozorista){
   for (let element in pozorista){
     pozoriste_card = document.createElement("a", "col-4");
     pozoriste_card.classList.add("card");
     pozoriste_card.addEventListener("click", function() {
-      localStorage.setItem("pozoriste", element)
+      localStorage.setItem("izabrano_pozoriste", element)
     })
     pozoriste_card.href = "pozoriste.html"
 
@@ -81,6 +81,80 @@ function prikazi_pozorista(pozorista){
     kartice.appendChild(pozoriste_card);
   }
 }
+
+//pozoriste.html
+function prikazi_predstave(predstave, id_pozorista){
+  let pozorista = JSON.parse(localStorage.getItem("pozorista"));
+  sifra_predstava = pozorista[id_pozorista].idPredstava;
+
+  //postavljanje slike i imena pozorista
+  let pozoriste_ime = document.getElementsByClassName("pozoriste-ime")[0];
+  pozoriste_ime.innerHTML = pozorista[id_pozorista].naziv;
+  let slika_pozoriste = document.getElementsByClassName("slika-pozoriste")[0];
+  slika_pozoriste.src = pozorista[id_pozorista].slika;
+  slika_pozoriste.alt = pozorista[id_pozorista].naziv;
+
+  //listanje svih predstava
+  let repertoar = document.getElementsByClassName("repertoar")[0];
+  for (let predstava_dat in predstave[sifra_predstava]){
+    let predstava = document.createElement("a");
+    predstava.className = "predstava";
+    predstava.addEventListener("click", function() {
+      localStorage.setItem("izabrana_predstava", predstave[sifra_predstava][predstava_dat]);
+    })
+    predstava.href = "predstava.html";
+
+    //postavljanje adrese
+    adresa = document.getElementsByClassName("footer-text")[0];
+    adresa.innerHTML = "Adresa<br>" + pozorista[id_pozorista].adresa;
+
+    //slika predstva
+    let slika_predstava = document.createElement("img");
+    slika_predstava.className = "slika-predstave";
+    slika_predstava.src = predstave[sifra_predstava][predstava_dat].slika;
+    slika_predstava.alt = predstave[sifra_predstava][predstava_dat].naziv;
+    predstava.appendChild(slika_predstava);
+
+    //predstava-sredina
+    let predstava_sredina = document.createElement("div");
+    predstava_sredina.className = "predstava-sredina";
+
+    let naziv_prestave = document.createElement("h3");
+    naziv_prestave.className = "naziv-predstave";
+    naziv_prestave.innerHTML = predstave[sifra_predstava][predstava_dat].naziv;
+    predstava_sredina.appendChild(naziv_prestave);
+
+    opis_predstave = document.createElement("p");
+    opis_predstave.className = "opis-predstave";
+    opis_predstave.innerHTML = predstave[sifra_predstava][predstava_dat].kratakOpis;
+    predstava_sredina.appendChild(opis_predstave);
+
+    predstava.appendChild(predstava_sredina);
+
+    //vl
+    let vl = document.createElement("div");
+    vl.className = "vl";
+
+    predstava.appendChild(vl);
+
+    //predstava-info
+    predstava_info = document.createElement("div");
+    predstava_info.className = "predstava-info";
+
+    opis = document.createElement("p");
+    opis.innerHTML = "Žanr: " + predstave[sifra_predstava][predstava_dat].zanr +
+                      "<br>Trajanje: " +  predstave[sifra_predstava][predstava_dat].tajanje + " minuta" +
+                      "<br>Cena: " + predstave[sifra_predstava][predstava_dat].cena + " dinara" +
+                      "<br>Ocena: " + predstave[sifra_predstava][predstava_dat].ocena;
+
+    predstava_info.appendChild(opis);
+
+    predstava.appendChild(predstava_info);
+    repertoar.appendChild(predstava);
+  }
+}
+
+//predstava
 
 
 // Pop-up za registraciju i za prijavu
